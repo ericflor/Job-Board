6676567a617d579bab5e22864bb5b154876c2a10
Eric Florence <ericflorence.dev@gmail.com>
2019-02-20 10:57:39 -0800

    update jobs controller, views, add user id migration, configure figaro and initializers for stripe payment system API keys

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,7 @@ Bundler.require(*Rails.groups)
 
 module JobBoard
   class Application < Rails::Application
+    config.application_name = 'Job Board'
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
