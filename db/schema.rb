af7122864caf05acafb4f7b9acdc877e0dc14f58
Eric Florence <ericflorence.dev@gmail.com>
2019-02-20 10:12:37 -0800

    update job and user models, g avatar uploader with migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_20_174812) do
+ActiveRecord::Schema.define(version: 2019_02_20_180543) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2019_02_20_174812) do
     t.string "apply_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "avatar"
   end
 
   create_table "users", force: :cascade do |t|
